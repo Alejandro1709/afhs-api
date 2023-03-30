34cd7481231f18ca0ef917570900c5432da4b614
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-03-30 12:02:09 -0500

    Add: Basic Server

--- src/routers/character.routes.ts
+++ src/routers/character.routes.ts
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import {
+  createCharacter,
+  deleteCharacter,
+  getCharacter,
+  getCharacters,
+  updateCharacter,
+} from '../controllers/character.controller';
 
 const router = Router();
 
-// TODO: Implement routes
+router.route('/').get(getCharacters).post(createCharacter);
+
+router.route('/:slug').get(getCharacter).patch(updateCharacter).delete(deleteCharacter);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,22 @@
-// TODO: Implement Server
+import express from 'express';
+import morgan from 'morgan';
+import characterRoutes from './routers/character.routes';
+import { PORT, NODE_ENV } from './config/secrets';
+
+const app = express();
+
+app.use(express.json());
+
+if (NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use('/api/v1/characters', characterRoutes);
+
+app.get('/', (_req, res) => {
+  res.status(404).send('Not found');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} in ${NODE_ENV} mode`);
+});
