0af3f20049c10b9a4aff65cd056f0c163c603416
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-03-31 10:05:03 -0500

    Add: Delete endpoint

--- src/controllers/character.controller.ts
+++ src/controllers/character.controller.ts
@@ -1,43 +1,44 @@
 import characters from '../data/characters';
+import Character from '../models/Character';
 import type { Request, Response } from 'express';
+import type ICharacter from '../types/character';
 
 export const getCharacters = async (_req: Request, res: Response) => {
-  res.status(200).json(characters);
+  try {
+    const characters: ICharacter[] = await Character.find();
+    res.status(200).json(characters);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ status: 'Failed', message: error });
+  }
 };
 
 export const getCharacter = async (req: Request, res: Response) => {
   const { slug } = req.params;
-  const character = characters.find((character) => character.slug === slug);
 
-  if (!character) {
-    res.status(404).json({ message: 'Character not found' });
+  try {
+    const character = await Character.findOne({ slug });
+
+    if (!character) {
+      return res.status(404).json({ message: 'Character not found' });
+    }
+
+    res.status(200).json(character);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ status: 'Failed', message: error });
   }
-
-  res.status(200).json(character);
 };
 
 export const createCharacter = async (req: Request, res: Response) => {
-  const { name, actor, image, birthdate, work, otherWork, status, gender, height, nicknames, catchphrases } = req.body;
+  try {
+    const savedCharacter = await Character.create(req.body);
 
-  const newCharacter = {
-    id: characters.length + 1,
-    name,
-    slug: name.toLowerCase().replace(/ /g, '-'),
-    actor,
-    image,
-    birthdate,
-    work,
-    otherWork,
-    status,
-    gender,
-    height,
-    nicknames,
-    catchphrases,
-  };
-
-  characters.push(newCharacter);
-
-  res.status(201).json(newCharacter);
+    res.status(201).json(savedCharacter);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ status: 'Failed', message: error });
+  }
 };
 
 export const updateCharacter = async (req: Request, res: Response) => {
@@ -48,7 +49,7 @@ export const updateCharacter = async (req: Request, res: Response) => {
   const character = characters.find((character) => character.slug === slug);
 
   if (!character) {
-    res.status(404).json({ message: 'Character not found' });
+    return res.status(404).json({ message: 'Character not found' });
   }
 
   const updatedCharacter = {
@@ -77,15 +78,18 @@ export const updateCharacter = async (req: Request, res: Response) => {
 export const deleteCharacter = async (req: Request, res: Response) => {
   const { slug } = req.params;
 
-  const character = characters.find((character) => character.slug === slug);
+  try {
+    const character = await Character.findOne({ slug });
 
-  if (!character) {
-    res.status(404).json({ message: 'Character not found' });
+    if (!character) {
+      return res.status(404).json({ message: 'Character not found' });
+    }
+
+    await character.deleteOne();
+
+    res.status(200).json({ message: 'Character deleted' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ status: 'Failed', message: error });
   }
-
-  const index = characters.findIndex((character) => character.slug === slug);
-
-  characters.splice(index, 1);
-
-  res.status(200).json({ message: 'Character deleted' });
 };
