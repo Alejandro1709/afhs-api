905fe26bf188244d54f5dc07b8bbe97e70708378
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-03-31 10:26:02 -0500

    Fix: Issue when slugifying

--- src/models/Character.ts
+++ src/models/Character.ts
@@ -1,53 +1,68 @@
 import mongoose, { Schema } from 'mongoose';
+import slugify from 'slugify';
 import type ICharacter from '../types/character';
 
-const characterSchema = new Schema<ICharacter>({
-  name: { type: String, required: true },
-  slug: { type: String },
-  actor: [
-    {
+const characterSchema = new Schema<ICharacter>(
+  {
+    name: { type: String, required: true },
+    slug: { type: String },
+    actor: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    image: { type: String },
+    birthdate: [
+      {
+        type: String,
+      },
+    ],
+    work: [
+      {
+        type: String,
+      },
+    ],
+    otherWork: [
+      {
+        type: String,
+      },
+    ],
+    status: {
       type: String,
+      enum: ['VIVO', 'MUERTO', 'LUCHITO', 'DESCONOCIDO', 'DESAPARECIDO'],
+      required: true,
+      default: 'DESCONOCIDO',
+    },
+    gender: {
+      type: String,
+      enum: ['HOMBRE', 'MUJER'],
       required: true,
     },
-  ],
-  image: { type: String },
-  birthdate: [
-    {
-      type: String,
-    },
-  ],
-  work: [
-    {
-      type: String,
-    },
-  ],
-  otherWork: [
-    {
-      type: String,
-    },
-  ],
-  status: {
-    type: String,
-    enum: ['VIVO', 'MUERTO', 'LUCHITO', 'DESCONOCIDO', 'DESAPARECIDO'],
-    required: true,
-    default: 'DESCONOCIDO',
+    height: { type: Number },
+    nicknames: [
+      {
+        type: String,
+      },
+    ],
+    catchphrases: [
+      {
+        type: String,
+      },
+    ],
   },
-  gender: {
-    type: String,
-    enum: ['HOMBRE', 'MUJER'],
-    required: true,
+  {
+    timestamps: true,
   },
-  height: { type: Number },
-  nicknames: [
-    {
-      type: String,
-    },
-  ],
-  catchphrases: [
-    {
-      type: String,
-    },
-  ],
+);
+
+characterSchema.pre('save', function (next) {
+  if (!this.isModified('name')) {
+    next();
+  }
+
+  this.slug = slugify(this.name).toLowerCase();
+  next();
 });
 
 const Character = mongoose.model<ICharacter>('Character', characterSchema);
