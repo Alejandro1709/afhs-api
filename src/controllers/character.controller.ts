c36480cd7c632ec70a13d4bc1133a4b39fdf66d8
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-04-01 09:42:10 -0500

    Change: Update endpoint

--- src/controllers/character.controller.ts
+++ src/controllers/character.controller.ts
@@ -1,4 +1,3 @@
-import characters from '../data/characters';
 import Character from '../models/Character';
 import type { Request, Response } from 'express';
 import type ICharacter from '../types/character';
@@ -42,37 +41,23 @@ export const createCharacter = async (req: Request, res: Response) => {
 };
 
 export const updateCharacter = async (req: Request, res: Response) => {
-  const { name, actor, image, birthdate, work, otherWork, status, gender, height, nicknames, catchphrases } = req.body;
+  try {
+    const { slug } = req.params;
 
-  const { slug } = req.params;
+    const character = await Character.findOneAndUpdate({ slug }, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-  const character = characters.find((character) => character.slug === slug);
+    if (!character) {
+      return res.status(404).json({ message: 'Character not found' });
+    }
 
-  if (!character) {
-    return res.status(404).json({ message: 'Character not found' });
+    res.status(200).json(character);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ status: 'Failed', message: error });
   }
-
-  const updatedCharacter = {
-    ...character,
-    name,
-    slug: name.toLowerCase().replace(/ /g, '-'),
-    actor,
-    image,
-    birthdate,
-    work,
-    otherWork,
-    status,
-    gender,
-    height,
-    nicknames,
-    catchphrases,
-  };
-
-  const index = characters.findIndex((character) => character.slug === slug);
-
-  characters[index] = updatedCharacter;
-
-  res.status(200).json(updatedCharacter);
 };
 
 export const deleteCharacter = async (req: Request, res: Response) => {
