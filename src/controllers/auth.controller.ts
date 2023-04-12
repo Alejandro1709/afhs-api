b4676e64d6835aa39371ac88844b438239120a30
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-04-12 11:35:56 -0500

    Add: Implement catchAsync to endpoints

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,58 +1,48 @@
 import User from "../models/User";
-import type { Request, Response } from "express";
 import generateToken from "../utils/generateToken";
+import catchAsync from "../utils/catchAsync";
+import type { Request, Response } from "express";
 
-export const loginUser = async (req: Request, res: Response) => {
+export const loginUser = catchAsync(async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
-  try {
-    const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-    if (!user || !(await user.matchPasswords(password))) {
-      return res.status(401).json({ message: "Invalid Credentials" });
-    }
+  if (!user || !(await user.matchPasswords(password))) {
+    return res.status(401).json({ message: "Invalid Credentials" });
+  }
 
-    res.status(200).json({
+  res.status(200).json({
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    token: generateToken(user.id),
+  });
+});
+
+export const registerUser = catchAsync(async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    return res.status(400).json({ message: "Email already in use" });
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
       name: user.name,
       email: user.email,
       role: user.role,
       token: generateToken(user.id),
     });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: error });
+  } else {
+    res.status(400).json({ message: "Invalid User Data" });
   }
-}
-
-export const registerUser = async (req: Request, res: Response) => {
-  const { name, email, password } = req.body;
-
-  try {
-
-    const userExists = await User.findOne({ email });
-
-    if (userExists) {
-      return res.status(400).json({ message: "Email already in use" });
-    }
-
-    const user = await User.create({
-      name,
-      email,
-      password,
-    });
-
-    if (user) {
-      res.status(201).json({
-        name: user.name,
-        email: user.email,
-        role: user.role,
-        token: generateToken(user.id),
-      });
-    } else {
-      res.status(400).json({ message: "Invalid User Data" });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: error });
-  }
-}
+});
