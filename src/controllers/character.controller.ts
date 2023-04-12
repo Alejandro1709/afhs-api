554bf15096f8a6d70bcb34f258c959cf117938e3
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-04-12 11:38:53 -0500

    Add: catchAsync to character controller

--- src/controllers/character.controller.ts
+++ src/controllers/character.controller.ts
@@ -1,89 +1,64 @@
 import Character from '../models/Character';
 import cloudinary from '../utils/cloudinary';
+import catchAsync from '../utils/catchAsync';
 import type { Request, Response } from 'express';
 import type ICharacter from '../types/character';
 
-export const getCharacters = async (_req: Request, res: Response) => {
-  try {
-    const characters: ICharacter[] = await Character.find();
-    res.status(200).json(characters);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ status: 'Failed', message: error });
-  }
-};
+export const getCharacters = catchAsync(async (_req: Request, res: Response) => {
+  const characters: ICharacter[] = await Character.find();
+  res.status(200).json(characters);
+});
 
-export const getCharacter = async (req: Request, res: Response) => {
+export const getCharacter = catchAsync(async (req: Request, res: Response) => {
   const { slug } = req.params;
 
-  try {
-    const character = await Character.findOne({ slug });
+  const character = await Character.findOne({ slug });
 
-    if (!character) {
-      return res.status(404).json({ message: 'Character not found' });
-    }
-
-    res.status(200).json(character);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ status: 'Failed', message: error });
+  if (!character) {
+    return res.status(404).json({ message: 'Character not found' });
   }
-};
 
-export const createCharacter = async (req: Request, res: Response) => {
-  try {
+  res.status(200).json(character);
+});
 
-    const result = await cloudinary.uploader.upload(req.body.image, {
-      folder: 'afhs'
-    })
+export const createCharacter = catchAsync(async (req: Request, res: Response) => {
+  const result = await cloudinary.uploader.upload(req.body.image, {
+    folder: 'afhs'
+  })
 
-    const savedCharacter = await Character.create({
-      ...req.body,
-      image: result.secure_url,
-    });
+  const savedCharacter = await Character.create({
+    ...req.body,
+    image: result.secure_url,
+  });
 
-    res.status(201).json(savedCharacter);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ status: 'Failed', message: error });
-  }
-};
+  res.status(201).json(savedCharacter);
+});
 
-export const updateCharacter = async (req: Request, res: Response) => {
-  try {
-    const { slug } = req.params;
-
-    const character = await Character.findOneAndUpdate({ slug }, req.body, {
-      new: true,
-      runValidators: true,
-    });
-
-    if (!character) {
-      return res.status(404).json({ message: 'Character not found' });
-    }
-
-    res.status(200).json(character);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ status: 'Failed', message: error });
-  }
-};
-
-export const deleteCharacter = async (req: Request, res: Response) => {
+export const updateCharacter = catchAsync(async (req: Request, res: Response) => {
   const { slug } = req.params;
 
-  try {
-    const character = await Character.findOne({ slug });
+  const character = await Character.findOneAndUpdate({ slug }, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-    if (!character) {
-      return res.status(404).json({ message: 'Character not found' });
-    }
-
-    await character.deleteOne();
-
-    res.status(200).json({ message: 'Character deleted' });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ status: 'Failed', message: error });
+  if (!character) {
+    return res.status(404).json({ message: 'Character not found' });
   }
-};
+
+  res.status(200).json(character);
+});
+
+export const deleteCharacter = catchAsync(async (req: Request, res: Response) => {
+  const { slug } = req.params;
+
+  const character = await Character.findOne({ slug });
+
+  if (!character) {
+    return res.status(404).json({ message: 'Character not found' });
+  }
+
+  await character.deleteOne();
+
+  res.status(200).json({ message: 'Character deleted' });
+});
